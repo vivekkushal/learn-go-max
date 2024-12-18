bfef72a98becd9985b93f3ce00c6e4deaff61c5b
vivekkushal <vivekjss1@gmail.com>
2024-12-18 08:15:50 +0530

    feat: Add struct methods, constructor

--- structs-go/structs.go
+++ structs-go/structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -12,32 +13,53 @@ type user struct {
 	createdAt time.Time
 }
 
+func (u user) outputUserDetails() {
+	// fmt.Println((*u).firstName, (*u).lastName, (*u).birthDate)
+	fmt.Println(u.firstName, u.lastName, u.birthDate) // shortcut allowed by GO
+}
+
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func newUser(firstName, lastName, birthdate string) (*user, error) {
+	// constructor validation (if needed)
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first name, last name and birthdate are required")
+	}
+
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (DD/MM/YYYY): ")
 
-	var appUser user
-	appUser = user{
-		firstName: userFirstName,
-		lastName:  userLastName,
-		birthDate: userBirthdate,
-		createdAt: time.Now(),
-	}
+	var appUser *user
+
+	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
 
 	// ... do something awesome with that gathered data!
 
-	outputUserDetails(&appUser)
-}
-
-func outputUserDetails(u *user) {
-	// fmt.Println((*u).firstName, (*u).lastName, (*u).birthDate)
-	fmt.Println(u.firstName, u.lastName, u.birthDate) // shortcut allowed by GO
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
