84fce2d595d9f070e15816cc3c3f184bb45727ff
vivekkushal <vivekjss1@gmail.com>
2024-12-19 00:02:01 +0530

    refactor: User package

--- structs-go/structs.go
+++ structs-go/structs.go
@@ -1,50 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
-
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
-
-func (u user) outputUserDetails() {
-	// fmt.Println((*u).firstName, (*u).lastName, (*u).birthDate)
-	fmt.Println(u.firstName, u.lastName, u.birthDate) // shortcut allowed by GO
-}
-
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthdate string) (*user, error) {
-	// constructor validation (if needed)
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("first name, last name and birthdate are required")
-	}
-
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthdate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (DD/MM/YYYY): ")
 
-	var appUser *user
+	var appUser *user.User
 
-	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+	appUser, err := user.NewUser(userFirstName, userLastName, userBirthdate)
 
 	// ... do something awesome with that gathered data!
 
@@ -52,9 +21,9 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 }
 
 func getUserData(promptText string) string {
---
+++ structs-go/user/user.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
+func (u *User) OutputUserDetails() {
+	// fmt.Println((*u).firstName, (*u).lastName, (*u).birthDate)
+	fmt.Println(u.firstName, u.lastName, u.birthDate) // shortcut allowed by GO
+}
+
+func (u *User) ClearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func NewUser(firstName, lastName, birthdate string) (*User, error) {
+	// constructor validation (if needed)
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first name, last name and birthdate are required")
+	}
+
+	return &User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
+	}, nil
+}
