4b68eca8e173f381dc9d890bfcf36b307e55a6b4
vivekkushal <vivekjss1@gmail.com>
2024-12-22 00:14:55 +0530

    Final commit structs

--- structs-go/user/user.go
+++ structs-go/user/user.go
@@ -14,6 +14,7 @@ type User struct {
 }
 
 // anonymous struct embedding - similar to class inheritance
+// anonymous struct embedding is used here to get access of 'firstName', 'lastName' etc fields of Admin user in the 'structs.go' file
 type Admin struct {
 	email    string
 	password string
@@ -64,6 +65,7 @@ func New(firstName, lastName, birthdate string) (*User, error) {
 // ------------------
 
 // named struct embedding - similar to class inheritance
+// in this case all Admin fields, including the User fields, will be unexposed in the 'structs.go' file
 // type Admin struct {
 // 	email    string
 // 	password string
@@ -83,4 +85,37 @@ func New(firstName, lastName, birthdate string) (*User, error) {
 // 			createdAt: time.Now(),
 // 		},
 // 	}
+// }
+
+// ------------------
+
+// named struct embedding - similar to class inheritance
+// in this case all Admin fields, including the User fields, will be unexposed in the 'structs.go' file
+// type Admin struct {
+// 	email    string
+// 	password string
+// 	User     User
+// }
+
+// 'admin' constructor function
+// not using pointer return feature this time just to have variety => copy of Admin struct will be created by this constructor function
+// func NewAdmin(email, password string) Admin {
+// 	return Admin{
+// 		email:    email,
+// 		password: password,
+// 		User: User{
+// 			firstName: "ADMIN",
+// 			lastName:  "ADMIN",
+// 			birthDate: "---",
+// 			createdAt: time.Now(),
+// 		},
+// 	}
+// }
+
+// In above, even if we capitalize the 'user' field to 'User' we have to use admin.User.OutputUserDetails() to access it in the 'structs.go' file which is annoying, so the best solution is to write as:
+
+// type Admin struct {
+// 	email    string
+// 	password string
+// 	User
 // }
