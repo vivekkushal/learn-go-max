19542419d2ee61d889d86489748570d5a04f1f48
vivekkushal <vivekjss1@gmail.com>
2024-12-22 08:25:54 +0530

    feat: Save note to json file

--- notes-go/main.go
+++ notes-go/main.go
@@ -19,6 +19,13 @@ func main() {
 		return
 	}
 	userNote.Display()
+	err = userNote.Save()
+	if err != nil {
+		fmt.Println("saving the note failed")
+		return
+	}
+
+	fmt.Println("saving the note succeeded!")
 }
 
 func getNoteData() (string, string) {
--- notes-go/note/note.go
+++ notes-go/note/note.go
@@ -1,29 +1,44 @@
 package note
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdAt time.Time
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
-func (note *Note) Display() {
-	fmt.Printf("Your note titled '%v' has the following content:\n\n%v\n", note.title, note.content)
+func (note Note) Display() {
+	fmt.Printf("Your note titled '%v' has the following content:\n\n%v\n", note.Title, note.Content)
 }
 
-func New(title, content string) (*Note, error) {
+func (note Note) Save() error {
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
+
+	// convert data to json
+	json, err := json.Marshal(note)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(fileName, json, 0644)
+}
+
+func New(title, content string) (Note, error) {
 	if title == "" || content == "" {
-		return &Note{}, errors.New("invalid input")
+		return Note{}, errors.New("invalid input")
 	}
 
-	return &Note{
-		title:     title,
-		content:   content,
-		createdAt: time.Now(),
+	return Note{
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
 	}, nil
 }
